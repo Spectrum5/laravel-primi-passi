8fed1b4eb84195fd9de6ef4c6f6960e6d132655f
Spectrum5 <toskusergio@gmail.com>
2023-03-08 15:06:32 +0100

    Esercizio finito

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=h1, initial-scale=1.0">
+        <title>Laravel Primi Passi</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>
