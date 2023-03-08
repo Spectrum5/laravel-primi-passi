23915f31cf2759ff3bd69e38f711a8645a0d1bba
Spectrum5 <toskusergio@gmail.com>
2023-03-08 22:57:46 +0100

    Aggiunto file style.css e bootstrap cdn

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,9 +4,20 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=h1, initial-scale=1.0">
+        <!-- style css -->
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+
         <title>Laravel Primi Passi</title>
     </head>
+
     <body>
         <h1>Hello World</h1>
+
+
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
