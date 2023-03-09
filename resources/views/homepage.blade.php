64ccd4c3eb10165bec03f0c1dc21539c778173f8
Spectrum5 <toskusergio@gmail.com>
2023-03-09 02:51:46 +0100

    Aggiunto style.css

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,37 +1,61 @@
 <!DOCTYPE html>
 <html lang="it">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=h1, initial-scale=1.0">
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
 
-        <!-- style css -->
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-        
-        <title>Laravel Primi Passi</title>
-    </head>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 
-    <body class="bg-dark">
-        <div class="container">
-            <div class="text-center text-white">
-                <h1>
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- style css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+
+    <title>Laravel Primi Passi</title>
+</head>
+
+<body class="bg-dark">
+    <div class="container vh-100 justify-content-center d-flex align-items-center">
+
+        <div class="row row-cols-2 justify-content-center d-flex align-items-center">
+
+            <div class="col-12 text-center text-white mb-4 position-relative">
+                <h1 class="text-center">
                     Hello World!
                 </h1>
+                <h1 class="laravel-absolute position-absolute translate-middle start-50 text-danger">
+                    in Laravel
+                </h1>
             </div>
-            
-            <nav>
-                <ul>
-                    <li><a href="/">home</a></li>
-                    <li><a href="/about">about</a></li>
-                    <li><a href="/contacts">contacts</a></li>
-                </ul>
-            </nav>
-
+            <div class="col mb-3">
+                <a href="/about" class="text-decoration-none">
+                    <div class="card border-0">
+                        <div class="card-body d-flex justify-content-between align-items-center">
+                            <h5 class="card-title mb-0 fw-bold">ABOUT</h5>
+                            <i class="fas fa-chevron-right"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col mb-3">
+                <a href="/contacts" class="text-decoration-none">
+                    <div class="card border-0">
+                        <div class="card-body d-flex justify-content-between align-items-center">
+                            <h5 class="card-title mb-0 fw-bold">CONTACTS</h5>
+                            <i class="fas fa-chevron-right"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
-        <!-- Bootstrap -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
+    </div>
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
 </html>
